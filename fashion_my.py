a749b6c0367b4ff7df003b99ca8c67fe4a34a7cc
VBB <librusekus35790@gmail.com>
2025-02-27 18:33:35 +0700

    smth

--- fashion_my.py
+++ fashion_my.py
@@ -4,6 +4,8 @@ from numpy.typing import NDArray
 from mnist_reader import load_mnist
 import numpy as np
 
+STEP_SIZE = 0.002
+
 class Layer:
     matrix = None
     bias = None
@@ -27,10 +29,10 @@ class Relu(Transformer):
     prev: NDArray
     
     def forward(self, x):
-        self.prev = NDArray(x)
+        self.prev = x.copy()
         return np.maximum(x, 0)
     def backward(self, x):
-        return np.where(self.prev > 0, 1, 0)
+        return x @ np.where(self.prev > 0, 1, 0)
 
 class Linear(Transformer):
     layer: Layer
@@ -40,14 +42,19 @@ class Linear(Transformer):
         self.layer = layer
     
     def forward(self, x):
-        self.prev = NDArray(x)
+        self.prev = x.copy()
         return x @ self.layer.matrix + self.layer.bias
     def backward(self, x):
-        return x @ self.prev
+        dW = np.outer(x, self.prev)
+        db = np.sum(x)
+        self.layer.bias -= STEP_SIZE * db
+        self.layer.matrix -= STEP_SIZE * dW
+        return dW
  
 
 class Model:
     transformers = []
+    loss = 0
     def __init__(self):
         self.transformers.append(Linear(Layer(28*28, 512)))
         self.transformers.append(Relu())
@@ -55,7 +62,7 @@ class Model:
         self.transformers.append(Relu())
         self.transformers.append(Linear(Layer(512, 10)))
     
-    def run(self, x, layer: Layer):
+    def run(self, x):
         for i in range(len(self.transformers)):
             x = self.transformers[i].forward(x)
 
@@ -63,7 +70,7 @@ class Model:
     
     def optimize(self, y, y_true):
         y_soft = softmax(y)
-        err = cross_entropy(y_soft, y_true)
+        self.loss = cross_entropy(y_soft, y_true)
         b = diff_cel(y_soft, y_true)
         
         for i in range(len(self.transformers)-1, -1, -1):
@@ -99,36 +106,40 @@ def SGD(image, label, res, lr, layer: Layer):
     layer.matrix -= lr * g_matrix.reshape(-1, 1)                  
     layer.bias -= lr * g_bias                             
 
-def train(imgs, labels, layer: Layer):
+def train(imgs, labels):
     iters = int(len(imgs)/BATCH_SIZE)
     test_loss, correct = 0, 0
     for i in range(0, iters*BATCH_SIZE, BATCH_SIZE):
         image = imgs[i:i+BATCH_SIZE] / 256.0
         label = labels[i: i+BATCH_SIZE]
 
-        res = model.run(image, layer)
-        ress = np.max(res, 1)
-        res = np.argmax(res, 1)
-        loss = loss_fn(ress, label)
+        res = model.run(image)
 
-        test_loss += loss.item()
-        correct += (res == label).sum().item()
+        y = np.zeros((len(label), 10), dtype=float)  # Создаем массив нулей
+        y[np.arange(len(label)), label] = 1  # Вставляем единицы по индексам
 
-        SGD(image, label, res, 0.001, layer)
+        model.optimize(y, res)
+
+
+        res = softmax(res)
+        loss = model.loss
+        test_loss += loss
+
+        correct += (np.argmax(res, 1) == label).sum().item()
+
         if i % (100*BATCH_SIZE) == 0:
-            loss, current = loss.item(), i
-            print(f"loss: {loss:>7f}  [{current:>5d}/{len(imgs):>5d}]")
+            #print(res- y)
+            print(f"loss: {loss:>7f}  [{i:>5d}/{len(imgs):>5d}]")
     return test_loss / iters, correct / len(imgs)
 
 BATCH_SIZE = 60
 
 imgs, labels = load_mnist('data/FashionMNIST/raw/')
 model = Model()
-layer = Layer(28*28, 512)
 
 epochs = 5
 for epoch  in range(epochs):
     print(f"Epoch {epoch+1}\n-------------------------------")
-    test_loss, correct = train(imgs, labels, layer)
+    test_loss, correct = train(imgs, labels)
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 print("Done!")
