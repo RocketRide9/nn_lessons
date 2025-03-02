d2336aadd97e921bddbe2e8a20f125f799a22fc3
VBB <librusekus35790@gmail.com>
2025-03-02 17:03:39 +0700

    Добавить реализацию на pytorch

--- fashion_my.py
+++ fashion_my.py
@@ -4,7 +4,7 @@ from numpy.typing import NDArray
 from mnist_reader import load_mnist
 import numpy as np
 
-STEP_SIZE = 0.002
+STEP_SIZE = 1e-3
 BATCH_SIZE = 60
 
 class Layer:
@@ -117,8 +117,8 @@ def train(imgs, labels):
 imgs, labels = load_mnist('data/FashionMNIST/raw/')
 model = Model()
 
-epochs = 30
-for epoch  in range(epochs):
+epochs = 5
+for epoch in range(epochs):
     print(f"Epoch {epoch+1}\n-------------------------------")
     test_loss, correct = train(imgs, labels)
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
